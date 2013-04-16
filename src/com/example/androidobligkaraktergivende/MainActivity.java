5b60406a5a2a333ddd082a8b7261bfb0b4c8b775
Jan Tore Karlsen <jt.karlsen@hotmail.com>
2013-04-16 12:22:57 +0200

    var add

--- src/com/example/androidobligkaraktergivende/MainActivity.java
+++ src/com/example/androidobligkaraktergivende/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	private String truth = "Håvard e ei pølse";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
