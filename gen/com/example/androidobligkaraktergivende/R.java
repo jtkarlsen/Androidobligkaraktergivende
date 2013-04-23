f67acb73dccb070416c06908568af895b9119d48
Håvard Rustad Olsen <haavard.olsen@live.com>
2013-04-23 14:46:01 +0200

    Reg funker

--- gen/com/example/androidobligkaraktergivende/R.java
+++ gen/com/example/androidobligkaraktergivende/R.java
@@ -8,24 +8,66 @@
 package com.example.androidobligkaraktergivende;
 
 public final class R {
+    public static final class array {
+        public static final int pref_interval_entries=0x7f050000;
+        public static final int pref_interval_values=0x7f050001;
+    }
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int color=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ic_map=0x7f020002;
+        public static final int ic_people=0x7f020003;
+        public static final int ic_settings=0x7f020004;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int fragment_container=0x7f0a0000;
+        public static final int listitem_layout=0x7f0a0004;
+        public static final int map_button_id=0x7f0a0007;
+        public static final int people_button_id=0x7f0a0008;
+        public static final int point_latitude=0x7f0a0003;
+        public static final int register_button=0x7f0a0002;
+        public static final int settings_id=0x7f0a0009;
+        public static final int user_color=0x7f0a0005;
+        public static final int username=0x7f0a0006;
+        public static final int username_field=0x7f0a0001;
+    }
+    public static final class integer {
+        public static final int pref_interval_default=0x7f060000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_register=0x7f030001;
+        public static final int map_layout=0x7f030002;
+        public static final int people_layout=0x7f030003;
+        public static final int people_listitem_layout=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f090000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_name=0x7f070000;
+        public static final int default_text=0x7f07000b;
+        public static final int map_label=0x7f070001;
+        public static final int people_label=0x7f070003;
+        public static final int pref_clear_all_data_label=0x7f070008;
+        public static final int pref_clear_all_data_summary=0x7f070010;
+        public static final int pref_clear_data_cat_label=0x7f070007;
+        public static final int pref_clear_data_cat_summary=0x7f07000f;
+        public static final int pref_clear_own_data_label=0x7f070009;
+        public static final int pref_clear_own_data_summary=0x7f070011;
+        public static final int pref_clear_remote_data_label=0x7f07000a;
+        public static final int pref_clear_remote_data_summary=0x7f070012;
+        public static final int pref_interval_label=0x7f070005;
+        public static final int pref_interval_summary=0x7f07000d;
+        public static final int pref_trace_label=0x7f070004;
+        public static final int pref_trace_summary=0x7f07000c;
+        public static final int pref_username_label=0x7f070006;
+        public static final int pref_username_summary=0x7f07000e;
+        public static final int register_button_label=0x7f070014;
+        public static final int setting_label=0x7f070002;
+        public static final int username_hint_label=0x7f070013;
     }
     public static final class style {
         /** 
@@ -49,10 +91,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f080001;
+    }
+    public static final class xml {
+        public static final int settings=0x7f040000;
     }
 }
--- src/com/example/androidobligkaraktergivende/MainActivity.java
+++ src/com/example/androidobligkaraktergivende/MainActivity.java
@@ -1,23 +1,66 @@
 package com.example.androidobligkaraktergivende;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends Activity {
 
-	private String truth = "HÂvard e ei p¯lse";
+@SuppressLint("NewApi") 
+public class MainActivity extends FragmentActivity{
+	
+	
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_main);
+		getSupportFragmentManager().beginTransaction()
+    	.replace(R.id.fragment_container, new MapFragment())
+    	.commit();
 	}
 
-	@Override
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB) @Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
+		if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB)
+		{
+			getActionBar().setDisplayShowHomeEnabled(false);
+		}
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.settings_id:
+			// starter settings aktivitet
+			Intent intent = new Intent(this, SettingsActivity.class);
+			startActivity(intent);
+
+			return true;
+		case R.id.map_button_id:
+			getSupportFragmentManager().beginTransaction()
+        	.replace(R.id.fragment_container, new MapFragment())
+        	.commit();
+			return true;
+		case R.id.people_button_id:
+			// Kaller p√• metode som starter en alarmManager og servicen
+			getSupportFragmentManager().beginTransaction()
+        	.replace(R.id.fragment_container, new PeopleFragment())
+        	.commit();
+
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 }
